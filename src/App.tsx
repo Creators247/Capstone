03f7b61aa4c8aadcd609bf9ae7f674f12c1541be
Great Amaizu <greatamaizu@gmail.com>
2024-02-18 16:45:46 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Landingpage from "./component/Landing-Page/Landingpage";
+
+function App() {
+  return (
+    <>
+      <Landingpage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Header/Header.tsx
@@ -0,0 +1,50 @@
+import "./header.css";
+import { useEffect } from "react";
+const NavbarIcon = () => {
+  const openNavBar = () => {
+    let navBar = document.getElementById("navicon");
+    navBar?.classList.toggle("opennavbar");
+
+    let navContanier = document.getElementsByClassName("nav-container")[0];
+    navContanier?.classList.toggle("open-nav-contanier");
+  };
+  return (
+    <section id="navicon" className="navicon" onClick={openNavBar}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </section>
+  );
+};
+
+function Header() {
+  useEffect(() => {
+    let fade = document.getElementsByClassName("nav-container")[0];
+    setTimeout(() => {
+      fade.style.visibility = "visible";
+    }, 1000);
+  }, []);
+  return (
+    <section id="header">
+      <h1 className="logo">CHATTER</h1>
+      <div className="nav-container" style={{ visibility: "hidden" }}>
+        {window.screen.width <= 1000 ? <h1 className="logo">CHATTER</h1> : null}
+
+        <nav>
+          <a>Home</a>
+          <a>About Us</a>
+          <a>Contact</a>
+          <a>Blogs</a>
+        </nav>
+
+        <div className="signin-buttons">
+          <button>Log in</button>
+          <button>Sign up</button>
+        </div>
+      </div>
+      {window.screen.width <= 1000 ? <NavbarIcon /> : null}
+    </section>
+  );
+}
+
+export default Header;

f83d84eaa59ab929bc0b4fd6d64ccf996a3f1eef
Great Amaizu <greatamaizu@gmail.com>
2024-03-21 09:22:43 +0100

    C:/Program Files/Git/

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,14 +1,14 @@
 import SignUp from "./SignUp/SignUp";
 import Feeds from "./Feeds/Feeds.tsx";
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "./landingpage/Landingpage";
+import Landingpage from "./landingpage/Landingpage.tsx";
 import UserAccount from "./Feeds/components/NavBar/components/UserAccount";
 
 const Home = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Landingpage />} />
         <Route path="/feeds" element={<Feeds />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/user-account" element={<UserAccount />} />
